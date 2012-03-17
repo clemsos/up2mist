503cb95697afb782f07bf9a36137d021b4b21e9a
Clement Renaud <clement.renaud@gmail.com>
2012-03-17 22:50:12 +0100

    fix names

--- inc/theme-options/theme-options.php
+++ inc/theme-options/theme-options.php
@@ -1,34 +1,34 @@
 <?php
 /**
- * _s Theme Options
+ * up2mist Theme Options
  *
- * @package _s
- * @since _s 1.0
+ * @package up2mist
+ * @since up2mist 1.0
  */
 
 /**
- * Register the form setting for our _s_options array.
+ * Register the form setting for our up2mist_options array.
  *
  * This function is attached to the admin_init action hook.
  *
- * This call to register_setting() registers a validation callback, _s_theme_options_validate(),
+ * This call to register_setting() registers a validation callback, up2mist_theme_options_validate(),
  * which is used when the option is saved, to ensure that our option values are complete, properly
  * formatted, and safe.
  *
  * We also use this function to add our theme option if it doesn't already exist.
  *
- * @since _s 1.0
+ * @since up2mist 1.0
  */
-function _s_theme_options_init() {
+function up2mist_theme_options_init() {
 
 	// If we have no options in the database, let's add them now.
-	if ( false === _s_get_theme_options() )
-		add_option( '_s_theme_options', _s_get_default_theme_options() );
+	if ( false === up2mist_get_theme_options() )
+		add_option( 'up2mist_theme_options', up2mist_get_default_theme_options() );
 
 	register_setting(
-		'_s_options',       // Options group, see settings_fields() call in _s_theme_options_render_page()
-		'_s_theme_options', // Database option, see _s_get_theme_options()
-		'_s_theme_options_validate' // The sanitization callback, see _s_theme_options_validate()
+		'up2mist_options',       // Options group, see settings_fields() call in up2mist_theme_options_render_page()
+		'up2mist_theme_options', // Database option, see up2mist_get_theme_options()
+		'up2mist_theme_options_validate' // The sanitization callback, see up2mist_theme_options_validate()
 	);
 
 	// Register our settings field group
@@ -36,123 +36,123 @@ function _s_theme_options_init() {
 		'general', // Unique identifier for the settings section
 		'', // Section title (we don't want one)
 		'__return_false', // Section callback (we don't want anything)
-		'theme_options' // Menu slug, used to uniquely identify the page; see _s_theme_options_add_page()
+		'theme_options' // Menu slug, used to uniquely identify the page; see up2mist_theme_options_add_page()
 	);
 
 	// Register our individual settings fields
 	add_settings_field(
 		'sample_checkbox', // Unique identifier for the field for this section
-		__( 'Sample Checkbox', '_s' ), // Setting field label
-		'_s_settings_field_sample_checkbox', // Function that renders the settings field
-		'theme_options', // Menu slug, used to uniquely identify the page; see _s_theme_options_add_page()
+		__( 'Sample Checkbox', 'up2mist' ), // Setting field label
+		'up2mist_settings_field_sample_checkbox', // Function that renders the settings field
+		'theme_options', // Menu slug, used to uniquely identify the page; see up2mist_theme_options_add_page()
 		'general' // Settings section. Same as the first argument in the add_settings_section() above
 	);
 
-	add_settings_field( 'sample_text_input', __( 'Sample Text Input', '_s' ), '_s_settings_field_sample_text_input', 'theme_options', 'general' );
-	add_settings_field( 'sample_select_options', __( 'Sample Select Options', '_s' ), '_s_settings_field_sample_select_options', 'theme_options', 'general' );
-	add_settings_field( 'sample_radio_buttons', __( 'Sample Radio Buttons', '_s' ), '_s_settings_field_sample_radio_buttons', 'theme_options', 'general' );
-	add_settings_field( 'sample_textarea', __( 'Sample Textarea', '_s' ), '_s_settings_field_sample_textarea', 'theme_options', 'general' );
+	add_settings_field( 'sample_text_input', __( 'Sample Text Input', 'up2mist' ), 'up2mist_settings_field_sample_text_input', 'theme_options', 'general' );
+	add_settings_field( 'sample_select_options', __( 'Sample Select Options', 'up2mist' ), 'up2mist_settings_field_sample_select_options', 'theme_options', 'general' );
+	add_settings_field( 'sample_radio_buttons', __( 'Sample Radio Buttons', 'up2mist' ), 'up2mist_settings_field_sample_radio_buttons', 'theme_options', 'general' );
+	add_settings_field( 'sample_textarea', __( 'Sample Textarea', 'up2mist' ), 'up2mist_settings_field_sample_textarea', 'theme_options', 'general' );
 }
-add_action( 'admin_init', '_s_theme_options_init' );
+add_action( 'admin_init', 'up2mist_theme_options_init' );
 
 /**
- * Change the capability required to save the '_s_options' options group.
+ * Change the capability required to save the 'up2mist_options' options group.
  *
- * @see _s_theme_options_init() First parameter to register_setting() is the name of the options group.
- * @see _s_theme_options_add_page() The edit_theme_options capability is used for viewing the page.
+ * @see up2mist_theme_options_init() First parameter to register_setting() is the name of the options group.
+ * @see up2mist_theme_options_add_page() The edit_theme_options capability is used for viewing the page.
  *
  * @param string $capability The capability used for the page, which is manage_options by default.
  * @return string The capability to actually use.
  */
-function _s_option_page_capability( $capability ) {
+function up2mist_option_page_capability( $capability ) {
 	return 'edit_theme_options';
 }
-add_filter( 'option_page_capability__s_options', '_s_option_page_capability' );
+add_filter( 'option_page_capability_up2mist_options', 'up2mist_option_page_capability' );
 
 /**
  * Add our theme options page to the admin menu.
  *
  * This function is attached to the admin_menu action hook.
  *
- * @since _s 1.0
+ * @since up2mist 1.0
  */
-function _s_theme_options_add_page() {
+function up2mist_theme_options_add_page() {
 	$theme_page = add_theme_page(
-		__( 'Theme Options', '_s' ),   // Name of page
-		__( 'Theme Options', '_s' ),   // Label in menu
+		__( 'Theme Options', 'up2mist' ),   // Name of page
+		__( 'Theme Options', 'up2mist' ),   // Label in menu
 		'edit_theme_options',                    // Capability required
 		'theme_options',                         // Menu slug, used to uniquely identify the page
-		'_s_theme_options_render_page' // Function that renders the options page
+		'up2mist_theme_options_render_page' // Function that renders the options page
 	);
 }
-add_action( 'admin_menu', '_s_theme_options_add_page' );
+add_action( 'admin_menu', 'up2mist_theme_options_add_page' );
 
 /**
- * Returns an array of sample select options registered for _s.
+ * Returns an array of sample select options registered for up2mist.
  *
- * @since _s 1.0
+ * @since up2mist 1.0
  */
-function _s_sample_select_options() {
+function up2mist_sample_select_options() {
 	$sample_select_options = array(
 		'0' => array(
 			'value' =>	'0',
-			'label' => __( 'Zero', '_s' )
+			'label' => __( 'Zero', 'up2mist' )
 		),
 		'1' => array(
 			'value' =>	'1',
-			'label' => __( 'One', '_s' )
+			'label' => __( 'One', 'up2mist' )
 		),
 		'2' => array(
 			'value' => '2',
-			'label' => __( 'Two', '_s' )
+			'label' => __( 'Two', 'up2mist' )
 		),
 		'3' => array(
 			'value' => '3',
-			'label' => __( 'Three', '_s' )
+			'label' => __( 'Three', 'up2mist' )
 		),
 		'4' => array(
 			'value' => '4',
-			'label' => __( 'Four', '_s' )
+			'label' => __( 'Four', 'up2mist' )
 		),
 		'5' => array(
 			'value' => '3',
-			'label' => __( 'Five', '_s' )
+			'label' => __( 'Five', 'up2mist' )
 		)
 	);
 
-	return apply_filters( '_s_sample_select_options', $sample_select_options );
+	return apply_filters( 'up2mist_sample_select_options', $sample_select_options );
 }
 
 /**
- * Returns an array of sample radio options registered for _s.
+ * Returns an array of sample radio options registered for up2mist.
  *
- * @since _s 1.0
+ * @since up2mist 1.0
  */
-function _s_sample_radio_buttons() {
+function up2mist_sample_radio_buttons() {
 	$sample_radio_buttons = array(
 		'yes' => array(
 			'value' => 'yes',
-			'label' => __( 'Yes', '_s' )
+			'label' => __( 'Yes', 'up2mist' )
 		),
 		'no' => array(
 			'value' => 'no',
-			'label' => __( 'No', '_s' )
+			'label' => __( 'No', 'up2mist' )
 		),
 		'maybe' => array(
 			'value' => 'maybe',
-			'label' => __( 'Maybe', '_s' )
+			'label' => __( 'Maybe', 'up2mist' )
 		)
 	);
 
-	return apply_filters( '_s_sample_radio_buttons', $sample_radio_buttons );
+	return apply_filters( 'up2mist_sample_radio_buttons', $sample_radio_buttons );
 }
 
 /**
- * Returns the default options for _s.
+ * Returns the default options for up2mist.
  *
- * @since _s 1.0
+ * @since up2mist 1.0
  */
-function _s_get_default_theme_options() {
+function up2mist_get_default_theme_options() {
 	$default_theme_options = array(
 		'sample_checkbox' => 'off',
 		'sample_text_input' => '',
@@ -161,27 +161,27 @@ function _s_get_default_theme_options() {
 		'sample_textarea' => '',
 	);
 
-	return apply_filters( '_s_default_theme_options', $default_theme_options );
+	return apply_filters( 'up2mist_default_theme_options', $default_theme_options );
 }
 
 /**
- * Returns the options array for _s.
+ * Returns the options array for up2mist.
  *
- * @since _s 1.0
+ * @since up2mist 1.0
  */
-function _s_get_theme_options() {
-	return get_option( '_s_theme_options', _s_get_default_theme_options() );
+function up2mist_get_theme_options() {
+	return get_option( 'up2mist_theme_options', up2mist_get_default_theme_options() );
 }
 
 /**
  * Renders the sample checkbox setting field.
  */
-function _s_settings_field_sample_checkbox() {
-	$options = _s_get_theme_options();
+function up2mist_settings_field_sample_checkbox() {
+	$options = up2mist_get_theme_options();
 	?>
 	<label for"sample-checkbox">
-		<input type="checkbox" name="_s_theme_options[sample_checkbox]" id="sample-checkbox" <?php checked( 'on', $options['sample_checkbox'] ); ?> />
-		<?php _e( 'A sample checkbox.', '_s' );  ?>
+		<input type="checkbox" name="up2mist_theme_options[sample_checkbox]" id="sample-checkbox" <?php checked( 'on', $options['sample_checkbox'] ); ?> />
+		<?php _e( 'A sample checkbox.', 'up2mist' );  ?>
 	</label>
 	<?php
 }
@@ -189,27 +189,27 @@ function _s_settings_field_sample_checkbox() {
 /**
  * Renders the sample text input setting field.
  */
-function _s_settings_field_sample_text_input() {
-	$options = _s_get_theme_options();
+function up2mist_settings_field_sample_text_input() {
+	$options = up2mist_get_theme_options();
 	?>
-	<input type="text" name="_s_theme_options[sample_text_input]" id="sample-text-input" value="<?php echo esc_attr( $options['sample_text_input'] ); ?>" />
-	<label class="description" for="sample-text-input"><?php _e( 'Sample text input', '_s' ); ?></label>
+	<input type="text" name="up2mist_theme_options[sample_text_input]" id="sample-text-input" value="<?php echo esc_attr( $options['sample_text_input'] ); ?>" />
+	<label class="description" for="sample-text-input"><?php _e( 'Sample text input', 'up2mist' ); ?></label>
 	<?php
 }
 
 /**
  * Renders the sample select options setting field.
  */
-function _s_settings_field_sample_select_options() {
-	$options = _s_get_theme_options();
+function up2mist_settings_field_sample_select_options() {
+	$options = up2mist_get_theme_options();
 	?>
-	<select name="_s_theme_options[sample_select_options]" id="sample-select-options">
+	<select name="up2mist_theme_options[sample_select_options]" id="sample-select-options">
 		<?php
 			$selected = $options['sample_select_options'];
 			$p = '';
 			$r = '';
 
-			foreach ( _s_sample_select_options() as $option ) {
+			foreach ( up2mist_sample_select_options() as $option ) {
 				$label = $option['label'];
 				if ( $selected == $option['value'] ) // Make default first in list
 					$p = "\n\t<option style=\"padding-right: 10px;\" selected='selected' value='" . esc_attr( $option['value'] ) . "'>$label</option>";
@@ -219,23 +219,23 @@ function _s_settings_field_sample_select_options() {
 			echo $p . $r;
 		?>
 	</select>
-	<label class="description" for="sample_theme_options[selectinput]"><?php _e( 'Sample select input', '_s' ); ?></label>
+	<label class="description" for="sample_theme_options[selectinput]"><?php _e( 'Sample select input', 'up2mist' ); ?></label>
 	<?php
 }
 
 /**
  * Renders the radio options setting field.
  *
- * @since _s 1.0
+ * @since up2mist 1.0
  */
-function _s_settings_field_sample_radio_buttons() {
-	$options = _s_get_theme_options();
+function up2mist_settings_field_sample_radio_buttons() {
+	$options = up2mist_get_theme_options();
 
-	foreach ( _s_sample_radio_buttons() as $button ) {
+	foreach ( up2mist_sample_radio_buttons() as $button ) {
 	?>
 	<div class="layout">
 		<label class="description">
-			<input type="radio" name="_s_theme_options[sample_radio_buttons]" value="<?php echo esc_attr( $button['value'] ); ?>" <?php checked( $options['sample_radio_buttons'], $button['value'] ); ?> />
+			<input type="radio" name="up2mist_theme_options[sample_radio_buttons]" value="<?php echo esc_attr( $button['value'] ); ?>" <?php checked( $options['sample_radio_buttons'], $button['value'] ); ?> />
 			<?php echo $button['label']; ?>
 		</label>
 	</div>
@@ -246,29 +246,29 @@ function _s_settings_field_sample_radio_buttons() {
 /**
  * Renders the sample textarea setting field.
  */
-function _s_settings_field_sample_textarea() {
-	$options = _s_get_theme_options();
+function up2mist_settings_field_sample_textarea() {
+	$options = up2mist_get_theme_options();
 	?>
-	<textarea class="large-text" type="text" name="_s_theme_options[sample_textarea]" id="sample-textarea" cols="50" rows="10" /><?php echo esc_textarea( $options['sample_textarea'] ); ?></textarea>
-	<label class="description" for="sample-textarea"><?php _e( 'Sample textarea', '_s' ); ?></label>
+	<textarea class="large-text" type="text" name="up2mist_theme_options[sample_textarea]" id="sample-textarea" cols="50" rows="10" /><?php echo esc_textarea( $options['sample_textarea'] ); ?></textarea>
+	<label class="description" for="sample-textarea"><?php _e( 'Sample textarea', 'up2mist' ); ?></label>
 	<?php
 }
 
 /**
- * Returns the options array for _s.
+ * Returns the options array for up2mist.
  *
- * @since _s 1.0
+ * @since up2mist 1.0
  */
-function _s_theme_options_render_page() {
+function up2mist_theme_options_render_page() {
 	?>
 	<div class="wrap">
 		<?php screen_icon(); ?>
-		<h2><?php printf( __( '%s Theme Options', '_s' ), get_current_theme() ); ?></h2>
+		<h2><?php printf( __( '%s Theme Options', 'up2mist' ), get_current_theme() ); ?></h2>
 		<?php settings_errors(); ?>
 
 		<form method="post" action="options.php">
 			<?php
-				settings_fields( '_s_options' );
+				settings_fields( 'up2mist_options' );
 				do_settings_sections( 'theme_options' );
 				submit_button();
 			?>
@@ -280,13 +280,13 @@ function _s_theme_options_render_page() {
 /**
  * Sanitize and validate form input. Accepts an array, return a sanitized array.
  *
- * @see _s_theme_options_init()
+ * @see up2mist_theme_options_init()
  * @todo set up Reset Options action
  *
- * @since _s 1.0
+ * @since up2mist 1.0
  */
-function _s_theme_options_validate( $input ) {
-	$output = $defaults = _s_get_default_theme_options();
+function up2mist_theme_options_validate( $input ) {
+	$output = $defaults = up2mist_get_default_theme_options();
 
 	// The sample checkbox should either be on or off
 	if ( ! isset( $input['sample_checkbox'] ) )
@@ -298,16 +298,16 @@ function _s_theme_options_validate( $input ) {
 		$output['sample_text_input'] = wp_filter_nohtml_kses( $input['sample_text_input'] );
 
 	// The sample select option must actually be in the array of select options
-	if ( array_key_exists( $input['sample_select_options'], _s_sample_select_options() ) )
+	if ( array_key_exists( $input['sample_select_options'], up2mist_sample_select_options() ) )
 		$output['sample_select_options'] = $input['sample_select_options'];
 
 	// The sample radio button value must be in our array of radio button values
-	if ( isset( $input['sample_radio_buttons'] ) && array_key_exists( $input['sample_radio_buttons'], _s_sample_radio_buttons() ) )
+	if ( isset( $input['sample_radio_buttons'] ) && array_key_exists( $input['sample_radio_buttons'], up2mist_sample_radio_buttons() ) )
 		$output['sample_radio_buttons'] = $input['sample_radio_buttons'];
 
 	// The sample textarea must be safe text with the allowed tags for posts
 	if ( isset( $input['sample_textarea'] ) )
 		$output['sample_textarea'] = wp_filter_post_kses($input['sample_textarea'] );
 
-	return apply_filters( '_s_theme_options_validate', $output, $input, $defaults );
+	return apply_filters( 'up2mist_theme_options_validate', $output, $input, $defaults );
 }
