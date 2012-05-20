f6c2bd456085d83c08562d2357134ca96c3dfaf7
Clement Renaud <clement.renaud@gmail.com>
2012-05-20 18:38:05 +0200

    added custom widgets

--- sidebar.php
+++ sidebar.php
@@ -6,7 +6,7 @@
  * @since up2mist 1.0
  */
 ?>
-			<div id="secondary" class="<?php echo UP2MIST_SECONDARY;?> widget-area" role="complementary">
+			<div id="secondary" class="<?php echo UP2MIST_SECONDARY;?>" role="complementary">
 				
 				
 				
@@ -23,69 +23,57 @@
 					</aside>
 					
 					
-					<div class="tabbable">
+					<aside id="search" class="widget widget_search">
+						<?php get_search_form( ); ?>
+					</aside>
 					
-					<ul class="nav nav-tabs">
-					  <li class="active"><a href="#sidebar-menu" data-toggle="tab">Menu</a></li>
-					  <li class=""><a href="#search" data-toggle="tab">Search</a></li>
-					  <li class=""><a href="#widgets" data-toggle="tab">Widgets</a></li>
-					</ul>
-					
-					
-					<div class="tab-content">
-					
-						<aside id="search" class="tab-pane widget widget_search">
-							<?php get_search_form( ); ?>
-						</aside>
-				
-						<?php wp_nav_menu( 
-							array(
-							'theme_location' => 'sidebar-menu',
-							'container' => false,
-							'items_wrap' => '
-								<aside id="sidebar-menu" class="active tab-pane %2$s">
-									<ul class="nav nav-list">
-									%3$s
-									</ul>
-								</aside>',
-							'walker' => new Fabric_Nav_Walker()
-							) 
-						);
-						?>
-					
+					<?php wp_nav_menu( 
+						array(
+						'theme_location' => 'sidebar-menu',
+						'container' => false,
+						'items_wrap' => '
+							<aside id="sidebar-menu" class="active tab-pane %2$s">
+								<ul class="nav nav-list">
+								%3$s
+								</ul>
+							</aside>',
+						'walker' => new Fabric_Nav_Walker()
+						) 
+					);
+					?>
 
-						
-						<div id="widgets" class="tab-pane">
+					<div id="top-widgets" class="widget-area">
 						<?php if ( ! dynamic_sidebar( 'sidebar-top' ) ) :?>
 							<?php if ( current_user_can( 'edit_posts' ) ) :?>
 							<?php printf( __( 'Please select some widgets for "SIDEBAR Top" <a href="%1$s">Get started here</a>.', 'up2mist' ), admin_url( 'widgets.php' ) );?>
 							<?php endif; // end sidebar top?>
 						<?php endif; // end sidebar top?>
-						</div>
-					</div><!-- end tab-content -->
-					</div><!-- end tabbable -->
+						<div class="clear">
+					</div>
 				
 				</section>
 				
-				
+				<section id="widget-sidebar" class="widget-area">
 				
 				<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) :
 				?>
-				<aside id="archives" class="widget">
-					<h1 class="widget-title"><?php _e( 'Archives', 'up2mist' );?></h1>
-					<ul>
-						<?php wp_get_archives( array( 'type' => 'monthly' ) );?>
-					</ul>
-				</aside>
-				<aside id="meta" class="widget">
-					<h1 class="widget-title"><?php _e( 'Meta', 'up2mist' );?></h1>
-					<ul>
-						<?php wp_register( );?>
-						<li>
-							<?php wp_loginout( );?>
-						</li>
-						<?php wp_meta( );?>
-					</ul>
-				</aside>
+				
+					<aside id="archives" class="widget">
+						<h1 class="widget-title"><?php _e( 'Archives', 'up2mist' );?></h1>
+						<ul>
+							<?php wp_get_archives( array( 'type' => 'monthly' ) );?>
+						</ul>
+					</aside>
+					<aside id="meta" class="widget">
+						<h1 class="widget-title"><?php _e( 'Meta', 'up2mist' );?></h1>
+						<ul>
+							<?php wp_register( );?>
+							<li>
+								<?php wp_loginout( );?>
+							</li>
+							<?php wp_meta( );?>
+						</ul>
+					</aside>
 				<?php endif; // end sidebar widget area?>
+				</section>
 			</div><!-- #secondary .widget-area -->
--- widgets.php
+++ widgets.php
@@ -1,142 +1,7 @@
-<?php
-/*
- * up2mist functions and definitions
- */
-
-/* Define theme constants */
-define( 'UP2MIST_URI', get_template_directory_uri( ) );
-define( 'UP2MIST_DIR', get_template_directory( ) );
-define( 'UP2MIST_RESPONSIVE', TRUE );
-define( 'UP2MIST_PRIMARY', 'span9' );
-define( 'UP2MIST_SECONDARY', 'span3' );
-define( 'UP2MIST_FULLWIDTH', 'span12' );
-define( 'UP2MIST_NAVCLASS', 'navbar' );
-
-/* Set the content width based on the theme's design and stylesheet. */
-if ( ! isset( $content_width ) )
-	$content_width = 770;
-/* pixels */
-
-add_action( 'after_setup_theme', 'up2mist_setup' );
-
-function up2mist_setup() {
-
-	/* Load up2mist template files */
-	/* locate_template( $template_names, $load, $require_once ) */
-	require_once (UP2MIST_DIR . '/inc/up2mist_hooks.php');
-	require_once (UP2MIST_DIR . '/inc/up2mist_functions.php');
-	require_once (UP2MIST_DIR . '/inc/up2mist_templates.php');
-	require_once (UP2MIST_DIR . '/inc/up2mist_navmenu.php');
-	require_once (UP2MIST_DIR . '/inc/up2mist_gallery.php');
-	//require_once (UP2MIST_DIR . '/inc/up2mist_shortcodes.php');
-
-	/* Make theme available for translation */
-	load_theme_textdomain( 'up2mist', get_template_directory( ) . '/languages' );
-
-	$locale = get_locale( );
-	$locale_file = get_template_directory( ) . "/languages/$locale.php";
-	if ( is_readable( $locale_file ) )
-		require_once ($locale_file);
-
-	/* Add default posts and comments RSS feed links to head */
-	add_theme_support( 'automatic-feed-links' );
-
-	/* Add suppot for post-thumbnails */
-	add_theme_support( 'post-thumbnails' );
-
-	/* This theme uses wp_nav_menu() in one location. */
-	register_nav_menus( 
-			array( 
-			'primary' => __( 'Primary Menu', 'up2mist' ), 
-			'sidebar-menu' => __( 'Sidebar Menu', 'up2mist' )
-			) 
-			);
-
-	/* Add support for the Aside and Gallery Post Formats */
-	add_theme_support( 'post-formats', array( 'aside', ) );
-
-	/* Add featured image size */
-	add_image_size( 'featured_fullwidth', 1170, 9999 );
-	add_image_size( 'featured_large', 770, 9999 );
-	
-	/* Add callback for custom TinyMCE editor stylesheets. */
-	add_editor_style('editor-style.css');
-
-}
-
-/* Load stylesheets */
-function up2mist_load_styleheets() {
-
-	$html = "\t" . '<link rel="stylesheet" href="' . UP2MIST_URI . '/css/bootstrap.css' . '">' . "\n";
-
-	if ( UP2MIST_RESPONSIVE ) {
-		$html .= "\t" . '<link rel="stylesheet" href="' . UP2MIST_URI . '/css/bootstrap-responsive.min.css' . '">' . "\n";
-	}
-
-	$html .= "\t" . '<link rel="stylesheet" href="' . UP2MIST_URI . '/style.css' . '">' . "\n";
-
-	echo $html;
-}
-
-add_action( 'up2mist_stylesheets', 'up2mist_load_styleheets' );
-
-/* Enqueue scripts and styles */
-function up2mist_enqueue_scripts() {
-
-	/* wp_enqueue_script( $handle, $src, $deps, $ver, $in_footer ); */
-	wp_enqueue_script( 'modernizr', UP2MIST_URI . '/js/modernizr.custom-2.5.3.min.js', false, '2.5.3' );
-	wp_enqueue_script( 'bootstrap', UP2MIST_URI . '/js/bootstrap.min.js', array( 'jquery' ), '2.0.2' );
-
-
-	// add custom script
-	wp_enqueue_script( 'script', UP2MIST_URI . '/js/script.js' );
-
-
-	if ( is_singular( ) && comments_open( ) && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
-	
-
-
-}
-
-add_action( 'wp_enqueue_scripts', 'up2mist_enqueue_scripts' );
-
-/* Register widgetized area and update sidebar with default widgets */
-function up2mist_widgets_init() {
-
-
-	register_sidebar( array(
-		'name' => __( 'Sidebar Top', 'up2mist' ),
-		'id' => 'sidebar-top',
-		'before_widget' => '	<aside id="%1$s" class="widget %2$s">
-					<ul class="nav nav-list">',
-		'after_widget' => "	</ul>
-					</aside>",
-		'before_title' => '<li class="nav-header">',
-		'after_title' => '</li><li class="divider"></li></ul>'
-	) );
-
-	register_sidebar( array(
-		'name' => __( 'Sidebar Bottom', 'up2mist' ),
-		'id' => 'sidebar-1',
-		'before_widget' => '	<aside id="%1$s" class="widget %2$s">
-					<ul class="nav nav-list">',
-		'after_widget' => "	</ul>
-					</aside>",
-		'before_title' => '<li class="nav-header">',
-		'after_title' => '</li><li class="divider"></li></ul>'
-	) );
-
-}
-
-add_action( 'widgets_init', 'up2mist_widgets_init' );
-
-/* add all specific widgets */
 class GetConnected extends WP_Widget {
 
     function GetConnected() {
-        parent::WP_Widget(false, $name = 'Custom Social Links');
+        parent::WP_Widget(false, $name = 'Sight Social Links');
     }
 
     function widget($args, $instance) {
@@ -389,7 +254,7 @@ add_action('widgets_init', create_function('', 'return register_widget("GetConne
 class Recentposts_thumbnail extends WP_Widget {
 
     function Recentposts_thumbnail() {
-        parent::WP_Widget(false, $name = 'Custom Recent Posts');
+        parent::WP_Widget(false, $name = 'Sight Recent Posts');
     }
 
     function widget($args, $instance) {
@@ -446,12 +311,3 @@ class Recentposts_thumbnail extends WP_Widget {
 }
 add_action('widgets_init', create_function('', 'return register_widget("Recentposts_thumbnail");'));
 
-
-/* change excerpt length */
-function custom_excerpt_length( $length ) {
-	return 20;
-}
-//add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
-
-
-?>
